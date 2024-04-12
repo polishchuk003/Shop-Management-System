7456c2e9d45c2dd61fdb3130557580b0b56db36a
polishchuk003 <serzik2303@gmail.com>
2024-04-12 13:30:33 +0300

    add migration

---
+++ DBContext/DataModel.cs
@@ -0,0 +1,26 @@
+﻿using Shop_Management_System.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Xml.Linq;
+
+
+namespace Shop_Management_System.DBContext
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext() : base("name=DataDbContext")
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+
+
+    }
+}
--- Model/Category.cs
+++ Model/Category.cs
@@ -10,6 +10,7 @@ namespace Shop_Management_System.Model
     {
         public int CategoryId { get; set; }
         public string Name { get; set; }
+        public List<Item> Items { get; set; }
 
     }
 }
